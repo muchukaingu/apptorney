c69485b8e3660e43a7a081befd58b184e8d64983
Muchu Kaingu <muchu@circuitbusiness.com>
2017-11-06 10:14:53 +0200

    select parent legislation bug

--- backend/frontend/app/scripts/templates/templates.js
+++ backend/frontend/app/scripts/templates/templates.js
@@ -1204,7 +1204,7 @@ angular.module('theme.templates', []).run(['$templateCache', function ($template
     "              </div>\n" +
     "              <div class=\"col-xs-12 col-md-12 form-group\">\n" +
     "                <ui-select ng-model=\"legislation.parentLegislation\" theme=\"selectize\" ng-model-options=\"{ debounce: 1000 }\">\n" +
-    "                    <ui-select-match placeholder=\"Search for Parent Legislation...\">{{$select.selected.legislationName + ' | <em>' + parent.legislationType + '</em>'}}</ui-select-match>\n" +
+    "                    <ui-select-match placeholder=\"Search for Parent Legislation...\">{{$select.selected.legislationName + ' | ' + $select.selected.legislationType }}</ui-select-match>\n" +
     "                        <ui-select-choices repeat=\"parent.id as parent in parents track by $index\"\n" +
     "                                 refresh=\"searchForParent($select.search)\"\n" +
     "                                 refresh-delay=\"0\">\n" +

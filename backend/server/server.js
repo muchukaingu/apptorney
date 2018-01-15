f45420314acfc6aea2b0fb70ebab9eb7476dee53
Muchu Kaingu <muchu@circuitbusiness.com>
2018-01-15 12:54:05 +0200

    loopback compression change

--- backend/server/server.js
+++ backend/server/server.js
@@ -7,7 +7,7 @@ var path = require('path');
 
 var app = module.exports = loopback();
 
-app.use(loopback.compress());
+//app.use(loopback.compress());
 
 app.use(loopback.static(path.resolve(__dirname, '../client')));
 
@@ -15,26 +15,26 @@ app.use(loopback.static(path.resolve(__dirname, '../client')));
 
 
 app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
+    // start the web server
+    return app.listen(function() {
+        app.emit('started');
+        var baseUrl = app.get('url').replace(/\/$/, '');
+        console.log('Web server listening at: %s', baseUrl);
+        if (app.get('loopback-component-explorer')) {
+            var explorerPath = app.get('loopback-component-explorer').mountPath;
+            console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+        }
 
 
-  });
+    });
 };
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
 boot(app, __dirname, function(err) {
-  if (err) throw err;
+    if (err) throw err;
 
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
+    // start the server if `$ node server.js`
+    if (require.main === module)
+        app.start();
 });

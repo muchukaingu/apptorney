b077c7236dd15681421cfac9f664d686564201ed
Muchu Kaingu <muchu@circuitbusiness.com>
2018-01-17 16:09:27 +0200

    Reference Duplicate Removal

--- backend/common/models/case-cases.js
+++ backend/common/models/case-cases.js
@@ -1,3 +1,53 @@
 module.exports = function(CaseCases) {
+    const { ObjectId } = require('mongodb') // or ObjectID
 
-};
+    /**
+     * Lists all duplicated cases with the number of duplicate occurences
+     *
+     * @callback {Function} cb The callback function
+     */
+    CaseCases.getDuplicates = function(cb) {
+        var collection = CaseCases.getDataSource().connector.collection('caseCases')
+        collection.aggregate([{
+                    '$group': {
+                        '_id': { 'caseId': '$caseId', 'caseReferedToId': '$caseReferedToId' },
+                        'uniqueIds': { '$addToSet': '$_id' },
+                        'count': { '$sum': 1 }
+                    }
+                },
+                { '$match': { 'count': { '$gt': 1 } } }
+            ],
+            function(err, refs) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    var toDelete = []
+                    console.log(refs.length)
+
+                    refs.forEach(function(refInstance) {
+                        for (var i = 0; i < refInstance.uniqueIds.length; i++) {
+                            if (i > 0) {
+
+                                toDelete.push(refInstance.uniqueIds[i])
+                                collection.remove({ _id: refInstance.uniqueIds[i] })
+                            }
+                        }
+
+                        refInstance.fields = refInstance._id
+                        delete refInstance['_id']
+                    })
+                    cb(null, toDelete.length)
+                }
+            })
+    }
+
+    CaseCases.remoteMethod(
+        'getDuplicates', {
+            http: { path: '/duplicates', verb: 'get' },
+            accepts: [],
+            returns: [
+                { arg: 'duplicates', type: 'Object' },
+                { arg: 'uniqueCount', type: 'Object' }
+            ]
+        })
+}

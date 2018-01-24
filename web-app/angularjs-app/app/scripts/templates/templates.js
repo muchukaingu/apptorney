ef1e5378347aabe890f84b47b1363c9dcbace06a
Muchu Kaingu <muchu@circuitbusiness.com>
2018-01-24 20:28:01 +0200

    Send Dist Dir

--- web-app/angularjs-app/app/scripts/templates/templates.js
+++ web-app/angularjs-app/app/scripts/templates/templates.js
@@ -794,7 +794,7 @@ angular.module('theme.templates', []).run(['$templateCache', function ($template
 
 
   $templateCache.put('templates/area-of-law-modal.html',
-    "<div id=\"addAreaOfLawModal\" class=\"modal fade\" style=\"z-index:3000; background-color:rgba(0, 0, 0, 0.5);\">\n" +
+    "<div id=\"addAreaOfLawModal\" class=\"modal  fade in\" style=\"z-index:3000; background-color:rgba(0, 0, 0, 0.5);\">\n" +
     "    <div class=\"modal-dialog\" style=\"width:50%;padding-left: 2%;padding-right: 2%; \">\n" +
     "        <div class=\"modal-content\" style=\"margin-top: 8%\">\n" +
     "            <div class=\"modal-header\" style=\"border-bottom:none\">\n" +

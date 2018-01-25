db87fb28f724fbfc5ffb354ad4f931c8c8bc2c10
Muchu Kaingu <muchu@circuitbusiness.com>
2018-01-25 10:25:03 +0200

    work search and index creation

--- backend/common/models/work.js
+++ backend/common/models/work.js
@@ -1,3 +1,37 @@
 module.exports = function(Work) {
+    /**
+     * Searches for Work based on mongo's $text index
+     *
+     * @callback {Function} cb The callback function
+     */
+    Work.flexisearch = function(term, cb) {
+        var workCollection = Work.getDataSource().connector.collection('work')
+        workCollection.createIndex({ name: "text" });
+        workCollection.aggregate([
+                { $match: { $and: [{ 'deleted': { $eq: !true } }, { $text: { $search: '"' + term + '"' } }] } },
+                {
+                    $project: {
+                        score: { $meta: 'textScore' },
+                        name: true
+                    }
+                },
+                { $sort: { score: { $meta: 'textScore' }, name: -1 } }
+
+            ],
+            function(err, works) {
+                // console.log(works)
+                if (err) {} else {
+                    cb(null, works)
+                }
+            })
+    }
+
+
+    Work.remoteMethod(
+        'flexisearch', {
+            http: { path: '/flexisearch', verb: 'get' },
+            accepts: { arg: 'term', type: 'string' },
+            returns: { arg: 'works', type: 'Object' }
+        });
 
 };

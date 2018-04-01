9b119bfa7efee96e1157da308e99fc6662de2f91
Muchu Kaingu <muchu@circuitbusiness.com>
2018-04-01 17:18:50 +0200

    news and trending toggling

--- backend/common/models/news.js
+++ backend/common/models/news.js
@@ -1,1 +1,83 @@
-module.exports = function(News) {}
+module.exports = function(News) {
+    const { ObjectId } = require('mongodb') // or ObjectID
+
+    News.remoteMethod(
+        'addNews', {
+            http: { path: '/addNews', verb: 'post' },
+            accepts: [
+                { arg: 'sourceId', type: 'string' },
+                { arg: 'type', type: 'string' }
+            ],
+            returns: { arg: 'news', type: 'Object' }
+        })
+
+    /**
+     * Add Bookmarks
+     * @param {String} id 
+     * @callback {Function} cb The callback function
+     */
+    News.addNews = function(sourceId, type, cb) {
+        var app = News.app
+        var Case = app.models.Case
+        var Legislation = app.models.Legislation
+            // Case Bookmarks
+
+        News.findOne({ where: { sourceId: ObjectId(sourceId) } }, function(err, newsItem) {
+            if (newsItem) {
+                console.log(newsItem)
+                News.destroyById(newsItem.id, function(err, deleted) {
+                    console.log(err, deleted)
+                    cb(err, deleted)
+                })
+            } else {
+                if (type == 'case') {
+                    Case.findOne({ where: { id: ObjectId(sourceId) } },
+                        function(err, instance) {
+                            if (instance == null) {
+                                console.log('case not found', sourceId)
+                                cb(err, null)
+                            } else {
+                                var news = {
+                                    title: instance.name,
+                                    summary: instance.summaryOfFacts,
+                                    sourceId: instance.id,
+                                    type: 'case'
+                                }
+                                News.create(news,
+                                    function(err, news) {
+                                        if (err) {
+                                            cb(err)
+                                        } else {
+                                            cb(null, news)
+                                        }
+                                    })
+                            }
+                        })
+                } else if (type == 'legislation') {
+                    Legislation.findOne({ where: { id: ObjectId(sourceId) } },
+                        function(err, instance) {
+                            if (instance == null) {
+                                console.log('legislation not found', sourceId)
+                                cb(err, null)
+                            } else {
+                                var news = {
+                                    title: instance.legislationName,
+                                    summary: instance.preamble,
+                                    sourceId: instance.id,
+                                    type: 'legislation'
+                                }
+                                News.create(news,
+                                    function(err, news) {
+                                        if (err) {
+                                            cb(err)
+                                        } else {
+                                            cb(null, news)
+                                        }
+                                    })
+                            }
+                        })
+                }
+            }
+        })
+    }
+}
--- backend/common/models/trending.js
+++ backend/common/models/trending.js
@@ -1,1 +1,83 @@
-module.exports = function(Trending) {}
+module.exports = function(Trending) {
+    const { ObjectId } = require('mongodb') // or ObjectID
+
+    Trending.remoteMethod(
+        'addTrend', {
+            http: { path: '/addTrend', verb: 'post' },
+            accepts: [
+                { arg: 'sourceId', type: 'string' },
+                { arg: 'type', type: 'string' }
+            ],
+            returns: { arg: 'trends', type: 'Object' }
+        })
+
+    /**
+     * Add Bookmarks
+     * @param {String} id 
+     * @callback {Function} cb The callback function
+     */
+    Trending.addTrend = function(sourceId, type, cb) {
+        var app = Trending.app
+        var Case = app.models.Case
+        var Legislation = app.models.Legislation
+            // Case Bookmarks
+
+        Trending.findOne({ where: { sourceId: ObjectId(sourceId) } }, function(err, trend) {
+            if (trend) {
+                console.log(trend)
+                Trending.destroyById(trend.id, function(err, deleted) {
+                    console.log(err, deleted)
+                    cb(err, deleted)
+                })
+            } else {
+                if (type == 'case') {
+                    Case.findOne({ where: { id: ObjectId(sourceId) } },
+                        function(err, instance) {
+                            if (instance == null) {
+                                console.log('case not found', sourceId)
+                                cb(err, null)
+                            } else {
+                                var trends = {
+                                    title: instance.name,
+                                    summary: instance.summaryOfFacts,
+                                    sourceId: instance.id,
+                                    type: 'case'
+                                }
+                                Trending.create(trends,
+                                    function(err, trends) {
+                                        if (err) {
+                                            cb(err)
+                                        } else {
+                                            cb(null, trends)
+                                        }
+                                    })
+                            }
+                        })
+                } else if (type == 'legislation') {
+                    Legislation.findOne({ where: { id: ObjectId(sourceId) } },
+                        function(err, instance) {
+                            if (instance == null) {
+                                console.log('legislation not found', sourceId)
+                                cb(err, null)
+                            } else {
+                                var trends = {
+                                    title: instance.legislationName,
+                                    summary: instance.preamble,
+                                    sourceId: instance.id,
+                                    type: 'legislation'
+                                }
+                                Trending.create(trends,
+                                    function(err, trends) {
+                                        if (err) {
+                                            cb(err)
+                                        } else {
+                                            cb(null, trends)
+                                        }
+                                    })
+                            }
+                        })
+                }
+            }
+        })
+    }
+}

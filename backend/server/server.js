8fe802f9646fb2fff7c70795fbcef1d65b3aa5d3
Muchu Kaingu <muchu@circuitbusiness.com>
2018-07-25 07:45:02 +0200

    cors experiments

--- backend/server/server.js
+++ backend/server/server.js
@@ -11,7 +11,7 @@ var cors = require('cors')
 
 //app.use(loopback.compress());
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://apptorney.circuitbusiness.com");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 })

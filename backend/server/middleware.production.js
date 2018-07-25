93ce4603aa9fdd54b3f995e93088f16f23ae5a3e
Muchu Kaingu <muchu@circuitbusiness.com>
2018-07-25 07:12:30 +0200

    more cors

--- backend/server/middleware.production.js
+++ backend/server/middleware.production.js
@@ -1,6 +1,6 @@
 var whitelist = ['https://apptorney.circuitbusiness.com', 'https://apptorney-cms.eu-gb.mybluemix.net'];
 var cors = require('cors')
 module.exports = function(app) {
-
+    app.use(cors())
     app.options('*', cors())
 }

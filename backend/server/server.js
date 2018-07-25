5574869c944d558c70d81c4962ffb911a7719d00
Muchu Kaingu <muchu@circuitbusiness.com>
2018-07-25 07:37:48 +0200

    Access-Control-Allow-Origin

--- backend/server/server.js
+++ backend/server/server.js
@@ -10,6 +10,11 @@ var app = module.exports = loopback();
 var cors = require('cors')
 
 //app.use(loopback.compress());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 
 

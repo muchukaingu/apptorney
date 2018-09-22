561b7d50f2c546060a4fa69ca2b03b2de0a65d1d
Muchu Kaingu <muchu@circuitbusiness.com>
2018-09-22 10:34:01 +0200

    add response handler

--- backend/common/models/response-handler.js
+++ backend/common/models/response-handler.js
@@ -1,28 +1,6 @@
 'use strict';
 
 module.exports = function(ResponseHandler) {
-    /*
-    ResponseHandler.sendCallback = function(success, statusCode, err, errMessage, successMessage, data, cb) {
-        let message = {};
-        message.success = success;
-        message.statusCode = statusCode;
-        if (statusCode === 200) {
-            message.message = successMessage;
-            message.data = data;
-            cb(null, message);
-        } else {
-
-            message.err = err;
-            message.message = errMessage;
-            console.log(message)
-
-            cb(new Error(errMessage))
-
-        }
-
-    }*/
-
-
     ResponseHandler.sendCallback = function(success, statusCode, err, errMessage, successMessage, data, cb) {
         console.log("sendCallback called" + err)
         let message = {};
@@ -37,5 +15,4 @@ module.exports = function(ResponseHandler) {
         }
         cb(null, message)
     }
-
 };
